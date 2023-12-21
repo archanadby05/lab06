4723b5c78b0223a9254491fb08a3c15d65e8bdfe
archanadby05 <archanadby05@gmail.com>
2023-12-21 11:08:07 +0530

    Add routes and views

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [HomeController::class, 'index'])->name('profile');
+
+Route::get('/contact', [HomeController::class, 'contact'])->name('contact');
+
+Route::post('/contact', [HomeController::class, 'create'])->name('contact.create');
